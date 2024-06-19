dda1b8eb3a9873bf0390541f7dbb91aac2d425b4
Ethan Gill <ethan.gill@ucalgary.ca>
2024-06-18 20:38:57 -0600

    feat: beautiful gallery grid styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./**/*.templ", "./**/*.go"],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        // Complex site-specific column configuration
-        gallery: "repeat( auto-fill, 24rem)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
